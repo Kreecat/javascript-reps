4bb5bf5e25ff96665dd9ce4ddc2b6d0831af60ba
Nate <sauceynate@gmail.com>
2017-02-06 16:15:59 -0600

    started hw, 1 and 2 done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var runTheJewels = ["rtj3", "muthatruckas", "rain man didn't die face down", "talk real good", "cause im smart and stuff"]
+
+function lengths(rtj){
+	for (i = 0; i < rtj.length; i++){
+		console.log(i)
+	}
+
+}
